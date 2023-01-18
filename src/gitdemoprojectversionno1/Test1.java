1e386ee46cf06ee7a391084cb47fea951c4d556f
Alisha <alishamishra.mit@gmail.com>
2023-01-19 04:25:32 +0530

    First commit......

---
+++ src/gitdemoprojectversionno1/Test1.java
@@ -0,0 +1,15 @@
+package gitdemoprojectversionno1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String s1= "I love India";
+		String [] s3=s1.split(" ");
+		for(int i=s3.length-1;i>=0;i--) {
+			
+			System.out.print(s3[i] + "   ");
+			
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module gitdemoprojectversionno1 {
+}
